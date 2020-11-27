fadd1416ae3cfdd3353ab82e56ef6c68653d0189
zhmhbest <zhmhbest@gmail.com>
2020-11-27 16:28:59 +0800

    enum

--- src/mod_contition.rs
+++ src/mod_contition.rs
@@ -62,4 +62,28 @@ pub fn helloMatch() {
     println!("{}", value_in_cents(Coin::Nickel));
     println!("{}", value_in_cents(Coin::Dime));
     println!("{}", value_in_cents(Coin::Quarter));
+
+    enum WebEvent {
+        PageLoad,
+        PageUnload,
+        KeyPress(char),
+        Paste(String),
+        Click { x: i64, y: i64 },
+    }
+    fn inspect(event: WebEvent) {
+        match event {
+            WebEvent::PageLoad => println!("page loaded"),
+            WebEvent::PageUnload => println!("page unloaded"),
+            WebEvent::KeyPress(c) => println!("pressed '{}'.", c),
+            WebEvent::Paste(s) => println!("pasted \"{}\".", s),
+            WebEvent::Click { x, y } => {
+                println!("clicked at x={}, y={}.", x, y);
+            },
+        }
+    }
+    inspect(WebEvent::PageLoad);
+    inspect(WebEvent::KeyPress('x'));
+    inspect(WebEvent::Paste("my text".to_owned()));
+    inspect(WebEvent::Click { x: 20, y: 80 });
+    inspect( WebEvent::PageUnload);
 }
